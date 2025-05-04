ac4ab26d1df50917fc35af84326e67613fd321f4
Edgar Sá <neto.edgarsa@gmail.com>
2025-05-04 14:47:58 -0300

    fix: ajuste no design da tela de login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -8,7 +8,6 @@ import {
   CardTitle
 } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
-import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
 export async function LoginForm({
@@ -24,13 +23,11 @@ export async function LoginForm({
       <Card>
         <CardHeader className="text-center flex flex-col justify-center align-middle">
           <div className="w-full h-auto flex align-middle justify-center">
-            <div className=" bg-green-500 rounded-sm pt-2 px-4">
-              <Image
-                src={'/imgs/logo.svg'}
-                alt="logo da mediz"
-                width={200}
-                height={200}
-              />
+            <div className=" bg-primary rounded-sm p-2 px-4">
+              <p className="text-primary-foreground font-bold text-4xl">
+                me<span className="uppercase">diz</span>
+                <span className="text-yellow-400">!</span>
+              </p>
             </div>
           </div>
           <CardTitle className="text-xl">Bem-vindo</CardTitle>
@@ -47,7 +44,11 @@ export async function LoginForm({
           >
             <div className="grid gap-6">
               <div className="flex flex-col gap-4">
-                <Button variant="outline" className="w-full" type="submit">
+                <Button
+                  variant="outline"
+                  className="w-full bg-primary hover:opacity-90 transition-opacity"
+                  type="submit"
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
